0dd3b958253875b904178f5499b857e05c3d1809
MarcosPotato <marquinhos2725@hotmail.com>
2022-07-26 16:32:15 -0300

    Adicionando Autenticação

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,29 @@
-import { useState } from 'react'
+import { signIn, useSession, signOut } from 'next-auth/react'
+
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
 import style from './style.module.scss'
 
 export function SignInButton(){
-    const [isUserLoggedIn, setIsUserLoggedIn] = useState<boolean>(true)
+    const { data, status } = useSession()
     
-    return isUserLoggedIn ? (
-        <button className={ style.signInButton } type="button">
+    return status === "authenticated" ? (
+        <button 
+            className={ style.signInButton } 
+            type="button"
+            onClick={() => signOut()}
+        >
             <FaGithub color="#04d361" />
-            teste
+                { data.user.name }
             <FiX style={{ marginLeft: "1rem" }} color="#737380" />
         </button >
     ): (
-        <button className={ style.signInButton } type="button">
+        <button 
+            className={ style.signInButton } 
+            type="button"
+            onClick={() => signIn("github")}
+        >
             <FaGithub color="#eba417" />
             Sign in with github
         </button >
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
+import { GetStaticProps } from "next"
 import Head from "next/head"
+
+import { stripe } from "../services/stipe"
+
 import { SubscribeButton } from "../components/SubscribeButton"
 
 import styles from "./home.module.scss"
 
-export default function Home() {
+interface HomeProps{
+  product: {
+    priceId: string
+    amount: string
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,12 +26,32 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90/month</span>
-            <SubscribeButton/>
+            <span>for { product.amount } month</span>
+            <SubscribeButton priceId={ product.priceId }/>
           </p>
         </section>
         <img src="/images/avatar.svg" alt="girl codding"/>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async() => {
+  const price = await stripe.prices.retrieve('price_1LPoqAJcnjImLdVAG970KAXw')
+  /* const price = await stripe.prices.retrieve('price_1LPoqAJcnjImLdVAG970KAXw', {
+    expand: ["product"]
+  }) */
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US',{
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100) //salvo em centavos
+  }
+  
+  return{
+    props: { product },
+    revalidate: 60 * 60 * 24, // 24 horas
+  }
 }
